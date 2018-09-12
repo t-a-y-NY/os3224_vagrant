9b7aa993df43e22f3cb341fc655256f0a599ed8b
t-a-y-NY <temyousafi@gmail.com>
2018-09-12 15:33:43 -0400

    Completed Part 3

--- xv6/sed.c
+++ xv6/sed.c
@@ -2,7 +2,7 @@
 #include "stat.h"
 #include "user.h"
 
-char buf[512];
+char buf[1024];
 
 void sed(int fd, char* name)
 {
@@ -11,21 +11,40 @@ void sed(int fd, char* name)
 
   int the_count = 0; // Transylvanian accent: "I am the_count."
 
-  while ((n = read(fd, buf, sizeof(buf))) > 0)
-  {
-    for (i = 0; i < n; i++)
-    {
-      if (buf[i] == 't' && buf[i+1] == 'h' && buf[i+2] == 'e')
+  if (fd < 0) {
+    for (i = 0; i < sizeof(name); i++) {
+      if (name[i] == 't' && name[i+1] == 'h' && name[i+2] == 'e') {
+        name[i] = 'x';
+        name[i+1] = 'y';
+        name[i+2] = 'z';
         the_count++;
+        printf(1, name);
+        printf(1, "\n");
+      }
     }
   }
-
-  if (n < 0) 
-  {
-    printf(1, "sed: read error\n");
-    exit();
+  else {
+    while ( (n = read(fd, buf, sizeof(buf) ) ) > 0) {
+      for (i = 0; i < n; i++) {
+        if (buf[i] == 't' && buf[i+1] == 'h' && buf[i+2] == 'e') {
+          buf[i] = 'x';
+          buf[i+1] = 'y';
+          buf[i+2] = 'z';
+          the_count++;
+          printf(1, buf);
+          printf(1, "\n");
+        }
+      }
+    }
   }
-  printf(1, "%d %s\n", the_count, name);
+  
+
+  // if (n < 0) 
+  // {
+  //   printf(1, "sed: read error\n");
+  //   exit();
+  // }
+  printf(1, "Found and replaced %d occurences.\n", the_count);
 }
 
 int main(int argc, char* argv[])
@@ -43,8 +62,8 @@ int main(int argc, char* argv[])
   {
     if ((fd = open(argv[i], 0)) < 0) 
     {
-      printf(1, "sed: cannot open %s\n", argv[i]);
-      exit();
+      // printf(1, "sed: cannot open %s\n", argv[i]);
+      // exit();
     }
     sed(fd, argv[i]);
     close(fd);
